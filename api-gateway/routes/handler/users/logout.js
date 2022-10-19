b2201a366a191d940d2bc845ba231cf1ccd58619
MuliaHartawan <mulia.hartawan011@gmail.com>
2022-10-19 16:01:04 +0700

    integration logout  user api gateway

--- api-gateway/routes/handler/users/logout.js
+++ api-gateway/routes/handler/users/logout.js
@@ -8,8 +8,9 @@ const api = apiAdapter(URL_SERVICE_USER);
 
 module.exports = async (req, res) => {
     try {
+
         const id = req.user.data.id;
-        const user = await api.get(`/users/${id}`, req.body);
+        const user = await api.post('/users/logout', { user_id : id });
         return res.json(user.data);
     
     } catch (error) {
