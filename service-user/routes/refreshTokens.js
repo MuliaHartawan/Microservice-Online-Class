e714a6464b3f468f3a6f74deb3304eb9aa8844fc
MuliaHartawan <mulia.hartawan011@gmail.com>
2022-10-19 08:56:38 +0700

    create get refresh tokens users on service users

--- service-user/routes/refreshTokens.js
+++ service-user/routes/refreshTokens.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const refreshTokenHandler = require('./handler/refresh-tokens')
 
 router.post('/', refreshTokenHandler.create);
+router.get('/', refreshTokenHandler.getToken);
 
 
 module.exports = router;
