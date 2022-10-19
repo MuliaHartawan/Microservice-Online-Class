be0d9946601af2c0e8fe1d83a4d3d08c6613fa8f
MuliaHartawan <mulia.hartawan011@gmail.com>
2022-10-19 15:34:58 +0700

    integration update users api gateway

--- api-gateway/routes/users.js
+++ api-gateway/routes/users.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 const usersHandler = require('./handler/users');
 
+const verifyToken = require('../middleware/verifyToken');
+
 router.post('/register', usersHandler.register);
 router.post('/login', usersHandler.login);
+router.put('/', verifyToken, usersHandler.update);
 
 module.exports = router;
